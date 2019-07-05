269a39755cb46bb331a6ebef5c02a2b43ae33cfe
nedflander <amir@afzali.ca>
2019-07-05 15:12:27 -0400

    Cleanup -> Public repo -> Some fixes

--- src/server/index.ts
+++ src/server/index.ts
@@ -6,4 +6,6 @@ const app = express();
 app.use(morgan('dev'));
 app.use('/api', require('./routes/log.ts'));
 
+app.use('/api', (req, res) => res.redirect('/'));
+
 app.listen(8080, () => console.log("Listening on 8080!"));
--- src/server/routes/log.ts
+++ src/server/routes/log.ts
@@ -8,10 +8,9 @@ router.get('/log', (req, res) => {
   const write = fs.createWriteStream(`${path.resolve('logs')}/access.log`, { flags: 'a' });
   write.on('open', () => {
     write.write(`Access: ${Date()}, IP: ${req.headers['cf-connecting-ip'] || req.headers['x-forwarded-for']} || ${req.connection.remoteAddress}\n`);
-    console.log('Access log!');
   })
     .on('error', err => console.log(err));
-  res.send('done');
+  res.sendStatus(200);
 });
 
 module.exports = router;
